30eb7eb60126e7fd07ea0a170464f8a178555758
AndrewI997 <102222540+AndrewI997@users.noreply.github.com>
2023-01-24 20:38:17 +0300

    config

--- dist/client/App.js
+++ dist/client/App.js
@@ -1,2 +1,9 @@
-const a = 1;
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const react_dom_1 = require("react-dom");
+const react_1 = require("react");
+const App = () => {
+    return (react_1.default.createElement("div", null, "App"));
+};
+(0, react_dom_1.render)(react_1.default.createElement(App, null), document.getElementById('app'));
 //# sourceMappingURL=App.js.map
